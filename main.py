a9e1a68a1d70915338b1c35b49b01588e1611a19
AvrahamBi <avrahambarilan@gmail.com>
2022-05-12 19:03:23 +0300

    see fields

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from pymongo import MongoClient
 import pymongo
-#import dnspython
+# pip install dnspython
 
-DB_NAME = "Cogni"
 USER = "admin"
 PASS = "admin"
 CONNECTION_STRING = "mongodb+srv://" + USER + ":" + PASS + "@cluster0.0anzo.mongodb.net/Cogni" #?retryWrites=true&w=majority"
-
 FIELDS = ["gender", "age", "sketchTime", "startTime", "count_path", "count_reset", "count_undo"] # TBD: path, label should be generated.
 
 
 def get_database():
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
 
-    # Create the database for our example (we will use the same database throughout the tutorial
-    clock = client.tests.clock.find({"date":1641314369550})
-    for c in clock:
-        print(c)
+    client = MongoClient(CONNECTION_STRING)
+    tests = client.tests.clock.find()
+
+    for test in tests:
+        print()
+        print("Test id:", test["_id"])
+        for field in FIELDS:
+            print(field, test[field])
 
 
 
